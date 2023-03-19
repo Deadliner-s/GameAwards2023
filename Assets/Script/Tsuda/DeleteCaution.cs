9844ba48805dc5539aa00862d892356c948c6ad7
Kurt0925 <kurtbega0925@gmail.com>
2023-03-19 21:02:40 +0900

    レーザーが出るようになりました

---
+++ Assets/Script/Tsuda/DeleteCaution.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteCaution : MonoBehaviour
+{
+    public float lifetime = 5f;  // オブジェクトの寿命（秒）
+    private float timer;  // タイマー
+
+    void Start()
+    {        
+        timer = lifetime;  // タイマーを設定する
+    }
+
+    void Update()
+    {        
+        timer -= Time.deltaTime;  // タイマーを減算する
+        if (timer <= 0)
+        {
+            Destroy(gameObject);  // オブジェクトを削除する
+        }
+    }
+}
+
