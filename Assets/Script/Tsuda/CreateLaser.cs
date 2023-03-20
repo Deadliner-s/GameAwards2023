36685615ae7ffc9103456f47f2620681d6d060c2
Kurt0925 <kurtbega0925@gmail.com>
2023-03-21 01:15:17 +0900

    レーザーにインターバルを追加しました

--- Assets/Script/Tsuda/CreateLaser.cs
+++ Assets/Script/Tsuda/CreateLaser.cs
@@ -5,14 +5,54 @@ using UnityEngine;
 public class CreateLaser : MonoBehaviour
 {
     public GameObject prefab; // プレハブオブジェクト
+    public float Interval = 2.0f;
+    public float Interval2 = 8.0f;
+
+    private float timer = 0.0f;
+    private float timer2 = 0.0f;
+    private bool wait = false;
+
+    private GameObject targetObject; // 対象オブジェクト
+
+    void Start()
+    {
+        // 対象オブジェクトを取得する
+        targetObject = GameObject.Find("AttackPhase");
+    }
 
     void Update()
     {
-        // もし'C'キーが押されたら
-        if (Input.GetKeyDown(KeyCode.C))
+        // 対象オブジェクトが存在しない場合は、処理を実行しない
+        if (targetObject == null)
         {
-            // プレハブオブジェクトを生成する
-            Instantiate(prefab, transform.position, transform.rotation);
+            return;
+        }
+
+        if (!wait)
+        {
+            timer += Time.deltaTime;
+            timer2 += Time.deltaTime;
+
+            if (timer >= Interval)
+            {
+                Instantiate(prefab, transform.position, transform.rotation); // プレハブオブジェクトを生成する
+                timer = 0.0f; // タイマーをリセットする
+            }
+            if (timer2 >= Interval2)
+            {
+                wait = true;
+                timer2 = 0.0f;
+            }
+        }
+        if (wait)
+        {
+            timer2 += Time.deltaTime;
+
+            if (timer2 >= Interval2)
+            {
+                wait = false;
+                timer2 = 0.0f;
+            }
         }
     }
 }
