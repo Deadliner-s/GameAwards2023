f0f9817d453fe6207dbc1b8c33dfa55e2847afbd
ジョンソン <nhs10517@nhs.hal.ac.jp>
2023-03-21 06:17:54 +0900

    ミサイルのUIを作りました

--- Assets/Script/Jonson/SpawnMissile2.cs
+++ Assets/Script/Jonson/SpawnMissile2.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class SpawnMissile2 : MonoBehaviour
 {
     public GameObject Missile;
     public float DestroyTime;
     public string Key;
+    GameObject obj;
 
     public float DelayTime = 3.0f;
 
+    Vector3 ToPos;              //î≠éÀêÊ
+  
     // Start is called before the first frame update
     void Start()
     {
-        
+        ToPos = GameObject.Find("Player").transform.position;
+
     }
 
     // Update is called once per frame
@@ -23,12 +26,14 @@ public class SpawnMissile2 : MonoBehaviour
         if (Input.GetKeyDown(Key))
         {
             Invoke("InstantiateObject", DelayTime);
-        }     
+            ToPos = GameObject.Find("Player").transform.position;
+        }
+
     }
 
     void InstantiateObject()
     {
-        GameObject obj = Instantiate(Missile, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        obj = Instantiate(Missile, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
         Destroy(obj, DestroyTime);
     }
 }
