168ea10f92c0de8fe5ef77b2a06f2f36fe228653
WNHS10220\morik <moriken060426@gmail.com>
2023-03-21 23:01:00 +0900

    ボスのHPゲージとダメージ判定が完成しました

---
+++ Assets/Script/moriken/BossHp.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//シーンの移動処理を行う機能
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class BossHp : MonoBehaviour
+{
+    GameObject Boss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Boss = GameObject.Find("Boss");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // もし衝突した相手オブジェクトのタグが"Enemy"ならば中の処理を実行
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Boss.GetComponent<MainBossHp>().Damage(collision);
+        }
+    }
+}
