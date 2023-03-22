76dfc9e78815876545cd954440ce276f6668b1c1
hami12 <s.shinya.1227@gmail.com>
2023-03-23 00:06:45 +0900

    プレイヤーミサイルフェイズ切り替え追加

--- Assets/Script/Sugisaka/Shot.cs
+++ Assets/Script/Sugisaka/Shot.cs
@@ -6,26 +6,29 @@ using UnityEngine.InputSystem;
 public class Shot : MonoBehaviour
 {
     public static Shot instance;
-
     Myproject InputActions;
 
-    // ミサイルのプレハブ
+    [Header("ミサイル発射用")]
+    [Tooltip("プレイヤーミサイルのプレハブ")]
     public GameObject MissilePrefab;
-
-    // 装填時間
+    [Tooltip("装填時間")]
     public int ReloadTime;
+    [Tooltip("ミサイル生成場所")]
+    public GameObject Shotpos;
     // 経過時間
     private int currenttime;
     // 射撃フラグ
     private bool Shotflg;
-    // ミサイル生成場所
-    public GameObject Shotpos;
 
+    //フェイズ切り替え用
+    [Header("フェイズ確認用オブジェクト")]
+    [SerializeField] GameObject PhaseObj;
+    private bool AtkPhaseFlg;
 
     // ロックオンされたオブジェクトのリスト
-    public List<GameObject> targets;// = new List<GameObject>();
-
-    private List<GameObject> sub = new List<GameObject>();
+    public List<GameObject> targets;
+    // 退避用
+    public List<GameObject> sub = new List<GameObject>();
 
     void Awake()
     {
@@ -35,16 +38,17 @@ public class Shot : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         currenttime = 0;    // 経過時間初期化
         Shotflg = false;    // フラグをオフに
 
-        
+        // フェイズ取得
+        AtkPhaseFlg = PhaseObj.activeSelf;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Shotflg == true)
         {
@@ -55,46 +59,61 @@ public class Shot : MonoBehaviour
                 Shotflg = false;
             }
         }
+
+        // フェイズ確認
+        AtkPhaseFlg = PhaseObj.activeSelf;
     }
 
     public void OnShot()
     {
-        if (Shotflg == false)
+        // フェイズの確認
+        if (AtkPhaseFlg == true)
         {
-            foreach(GameObject k in targets)
+            // アタックフェイズ    
+            // フラグの確認
+            if (Shotflg == false)
             {
-                sub.Add(k);
-            }
-
-            Vector3 PlayerPos = Shotpos.transform.position;
-
-            targets = new List<GameObject>();
-
-            // 配列にタグがTargetのオブジェクトを入れる
-            GameObject[] targetsObj = GameObject.FindGameObjectsWithTag("Target");
-            foreach (GameObject target in targetsObj)
-            {
-                // ターゲットがリストに含まれていなければ追加する
-                if (!targets.Contains(target))
+                // ターゲットリストをサブに退避
+                foreach (GameObject k in targets)
                 {
-                    targets.Add(target);
+                    // ターゲットがリストに含まれていなければ追加する
+                    if (!sub.Contains(k))
+                    {
+                        sub.Add(k);
+                    }
                 }
+
+                // 生成場所取得
+                Vector3 PlayerPos = Shotpos.transform.position;
+                // リスト再生成
+                targets = new List<GameObject>();
+
+                // 配列にタグがTargetのオブジェクトを入れる
+                GameObject[] targetsObj = GameObject.FindGameObjectsWithTag("Target");
+                foreach (GameObject target in targetsObj)
+                {
+                    // ターゲットがリストに含まれていなければ追加する
+                    if (!targets.Contains(target))
+                    {
+                        targets.Add(target);
+                    }
+                }
+
+                // 各ロックオンされたターゲットにミサイルを打つ
+                foreach (GameObject target in targets)
+                {
+                    // 新しい誘導ミサイルプレハブをインスタンス化する
+                    GameObject missile = Instantiate(MissilePrefab, PlayerPos, Quaternion.identity);
+
+                    // 誘導ミサイルのターゲットを設定する
+                    missile.GetComponent<TrackingBullet>().SetTarget(target);
+                }
+
+                // 経過時間初期化
+                currenttime = 0;
+                // 射撃フラグを立てる
+                Shotflg = true;
             }
-
-            // 各ロックオンされたターゲットにミサイルを打つ
-            foreach (GameObject target in targets)
-            {
-                // 新しい誘導ミサイルプレハブをインスタンス化する
-                GameObject missile = Instantiate(MissilePrefab, PlayerPos, Quaternion.identity);
-
-                // 誘導ミサイルのターゲットを設定する
-                missile.GetComponent<TrackingBullet>().SetTarget(target);
-            }
-
-            currenttime = 0;
-
-            Shotflg = true;
         }
-        
     }
 }
--- Assets/Script/Sugisaka/TrackingBullet.cs
+++ Assets/Script/Sugisaka/TrackingBullet.cs
@@ -7,45 +7,46 @@ public class TrackingBullet : MonoBehaviour
     public float Speed;                 // ミサイルの速度
     public float MaxSpeed;              // ミサイルの最高速度
     public float Accel;                 // 加速度
-    private GameObject target;     // 追跡する対象  
+    private GameObject target;          // 追跡する対象  
     public float maxFlightTime = 100f;  // ミサイルの最大飛行時間
     private float flightTime;           // ミサイルの現在の飛行時間
-    public int State = 0;              // 状態(0:横移動  1:追跡)
-
-
-    Vector3 Move;               //移動方向
-    Vector3 LateMove;           //滑らか動きをするためのmove変数
+    public int State = 0;               // 状態(0:横移動  1:追跡)
+    private Vector3 Move;               //移動方向
+    private Vector3 LateMove;           //滑らか動きをするためのmove変数
     private float off = 0.2f;
     private Quaternion rot;
 
     private void Start()
     {
-        // ミサイルのRigidbodyコンポーネントを取得する
-        //rb = GetComponent<Rigidbody>();
-
         // 存在時間初期化
         flightTime = 0;
 
         // 状態設定
         State = 0;
-
-
     }
     
     //private void FixedUpdate()
     private void Update()
     {
-        // 追跡対象が存在しないか、ミサイルの最大飛行時間を超えたかどうかをチェックする
-        //if (target == null || flightTime > maxFlightTime)
+        // ミサイルの最大飛行時間を超えたかどうか
         if (flightTime > maxFlightTime)
         {
             // ミサイルを破壊してメソッドを終了する
             DestroyMissile();
             return;
         }
+        // 追跡対象が存在しないか
         if (target == null)
         {
+            // 対象がいなかったら直線移動
             State = 2;
+        }
+
+        // スピード更新
+        Speed += Accel;
+        if (Speed >= MaxSpeed)
+        {
+            Speed = MaxSpeed;
         }
 
 
@@ -68,28 +69,12 @@ public class TrackingBullet : MonoBehaviour
 
             case (1):
                 // 誘導移動
-
-                // スピードに加速度を加算
-                Speed += Accel;
-                if (Speed >= MaxSpeed)
-                {
-                    Speed = MaxSpeed;
-                }
-
-                //// ミサイルから追跡対象への方向を計算する
-                //Vector3 targetDirection = (target.position - transform.position).normalized;
-                //// ミサイルが追跡対象を向くために必要な回転を計算する
-                //Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
-                //// Slerp補間を使用してミサイルを追跡対象に向けて回転する
-                //rb.rotation = Quaternion.Slerp(rb.rotation, targetRotation, turnSpeed * Time.deltaTime);
-                //// ミサイルを追跡対象に向かって移動させる
-                //rb.velocity = transform.forward * Speed;
-
+                
                 Move = target.transform.position - transform.position;
                 Move = Move.normalized;
                 LateMove = (Move - LateMove) * off + (LateMove);
 
-                // 飛行時間を増やす
+                // 飛行時間更新
                 flightTime++;
                 break;
 
@@ -103,6 +88,7 @@ public class TrackingBullet : MonoBehaviour
                 break;
         }
 
+        // 座標,回転更新
         Quaternion rot = Quaternion.FromToRotation(new Vector3(0.0f, 1.0f, 0.0f), LateMove);
         transform.rotation = rot;
         transform.position += LateMove * Speed;
@@ -112,6 +98,7 @@ public class TrackingBullet : MonoBehaviour
     // ミサイルがオブジェクトに衝突したときに呼び出される
     private void OnCollisionEnter(Collision collision)
     {       
+        // タグ名と違ったら
         if (collision.gameObject.tag != "PlayerBullet")
         {
             DestroyMissile();
