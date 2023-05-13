28e379baa5906ded77f0c64dc33e0929b7a52962
hami12 <s.shinya.1227@gmail.com>
2023-05-13 23:33:26 +0900

    確認追加ウィンドウはあとで

--- Assets/Script/Sugisaka/CheckCursor.cs
+++ Assets/Script/Sugisaka/CheckCursor.cs
@@ -5,25 +5,40 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuCursor_Resu : MonoBehaviour
+public class CheckCursor : MonoBehaviour
 {
+    public enum Stage
+    {
+        STAGE_1,
+        STAGE_2,
+        STAGE_3
+    }
+
     private Myproject InputActions;
-    
+
     private int Selected;
     private int ItemMax;
 
     [SerializeField]
-    private GameObject fade;                // フェードオブジェクト
-   
+    [Header("メニュー")]
+    private GameObject Menu;            // メニューのオブジェクト
+
     [Header("ゲームマネージャオブジェクト")]
     GameObject ManagerObj;
+
+    [SerializeField]
+    private GameObject fade;
+
+    [SerializeField]
+    [Header("Debug用初期ステージ選択")]
+    public Stage stage;
 
     void Awake()
     {
         InputActions = new Myproject();
         InputActions.Enable();
-        InputActions.UI.Up.performed += context => OnUp();
-        InputActions.UI.Down.performed += context => OnDown();
+        InputActions.UI.Left.performed += context => OnLeft();
+        InputActions.UI.Right.performed += context => OnRight();
         InputActions.UI.Select.performed += context => OnSelect();
     }
 
@@ -32,21 +47,25 @@ public class MenuCursor_Resu : MonoBehaviour
     {
         // マネージャオブジェクト取得
         ManagerObj = GameObject.Find("GameManager");
-        
+
         ItemMax = transform.childCount - 1;
-        
-        // 初期カーソル位置設定
+
         Selected = 0;
         // 初期選択を白に
-        transform.GetChild(Selected).GetComponent<TextMeshProUGUI>().color = Color.white;
+        transform.GetChild(Selected).GetComponent<TextMeshProUGUI>().color 
+            = Color.white;
     }
 
     // Update is called once per frame
     void Update()
-    {        
+    {
+        if (Menu.activeSelf == false)
+        {
+            InputActions.Enable();
+        }
     }
 
-    private void OnUp()
+    private void OnLeft()
     {
         // 前選択を黒に
         transform.GetChild(Selected).GetComponent<TextMeshProUGUI>().color = Color.black;
@@ -59,12 +78,12 @@ public class MenuCursor_Resu : MonoBehaviour
         SoundManager.instance.PlaySE("Select");
     }
 
-    private void OnDown()
+    private void OnRight()
     {
         // 前選択を黒に
         transform.GetChild(Selected).GetComponent<TextMeshProUGUI>().color = Color.black;
-        //
-        Selected++;
+        // 
+        Selected--;
         Selected = (int)Mathf.Repeat(Selected, ItemMax);
         // 現選択を白に
         transform.GetChild(Selected).GetComponent<TextMeshProUGUI>().color = Color.white;
@@ -74,35 +93,39 @@ public class MenuCursor_Resu : MonoBehaviour
 
     private void OnSelect()
     {
-        // 選択されたメニューによって処理を変える
         switch (Selected)
         {
             case (0):
-                // CONTINUE
-                int num = ManagerObj.GetComponent<GManager>().GetNowStage();
+                // No
+
+                // タイトルに戻る
+                Selected = 0;
+                this.gameObject.SetActive(false);
                 InputActions.Disable();
-                switch (num)
-                {
-                    case (0):
-                        // ステージ1
-                        fade.GetComponent<Fade>().StartCoroutine("Color_FadeOut", "Prologue");
-                        break;
-                    case (1):
-                        // ステージ2
-                        fade.GetComponent<Fade>().StartCoroutine("Color_FadeOut", "Stage2Event");
-                        break;
-                    case (2):
-                        // ステージ3
-                        fade.GetComponent<Fade>().StartCoroutine("Color_FadeOut", "Stage3Event");
-                        break;
-                }
+                Menu.SetActive(true);
                 SoundManager.instance.PlaySE("Decision");
                 break;
             case (1):
-                // Return to title
+                // Yes
+
                 InputActions.Disable();
-                fade.GetComponent<Fade>().StartCoroutine("Color_FadeOut", "Title");
+                ManagerObj.GetComponent<GManager>().ReSetData();
+                // デバッグ用?
+                if (stage == Stage.STAGE_1)
+                {
+                    //SceneManager.LoadScene("Stage1");
+                    fade.GetComponent<Fade>().StartCoroutine("Color_FadeOut", "Prologue");
+                }
+                if (stage == Stage.STAGE_2)
+                {
+                    fade.GetComponent<Fade>().StartCoroutine("Color_FadeOut", "Stage2Event");
+                }
+                if (stage == Stage.STAGE_3)
+                {
+                    fade.GetComponent<Fade>().StartCoroutine("Color_FadeOut", "Stage3Event");
+                }
                 SoundManager.instance.PlaySE("Decision");
+                //
                 break;
         }
     }
