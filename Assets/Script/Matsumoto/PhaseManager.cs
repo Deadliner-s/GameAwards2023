be8f3e5dd35c3327b1e8df3ae5bf68d80ee34923
WNHS10220\morik <moriken060426@gmail.com>
2023-05-15 15:42:48 +0900

    エフェクトマネージャ作成

--- Assets/Script/Matsumoto/PhaseManager.cs
+++ Assets/Script/Matsumoto/PhaseManager.cs
@@ -31,27 +31,6 @@ public class PhaseManager : MonoBehaviour
     [Tooltip("照準")]
     public GameObject Reticle;
 
-    [Tooltip("集中線")]
-    public GameObject Line;
-    private Animator LineAnime;
-
-    [Tooltip("ブラー")]
-    [SerializeField]
-    private Volume volume;
-    private RadialBlurVolume Blur;
-    private GaussianBlurVolume gBlur;
-    public float BlurStrength;
-    public float gBlurStrength;
-    private float BlurCount;
-    private float CountPoint = 0.001f;
-    private int i;
-
-    [Tooltip("マニューバ中ブラーに関するオブジェクト")]
-    [SerializeField]
-    private GameObject player;
-    private PlayerMove playerMove;
-    private bool ManeverEnd;
-
     [Header("デバッグ用 フェイズを固定する")]
     public bool Debug_FixPhaseFlg = false;
 
@@ -80,36 +59,6 @@ public class PhaseManager : MonoBehaviour
         nextPhase = currentPhase;
 
         vibrationManager = GameObject.Find("VibrationManagerObj");
-        if (Line)
-        {
-            LineAnime = Line.GetComponent<Animator>();
-            Line.SetActive(false);
-            LineAnime.SetBool("isMove", false);
-        }
-        else
-        {
-            Debug.Log("Lineがありません");
-        }
-
-        // ブラーの初期化処理
-        if(volume)
-        {
-            volume.profile.TryGet(out Blur);
-            volume.profile.TryGet(out gBlur);
-            BlurCount = BlurStrength * 1000;
-            Blur.strength.value = 0.0f;
-            gBlur.SamplingDistance.value = 0.0f;
-        }
-        else
-        {
-            Debug.Log("volumeがありません");
-        }
-
-        playerMove = player.GetComponent<PlayerMove>();
-        if(playerMove == null)
-        {
-            Debug.Log("playerがありません");
-        }
     }
 
     // Update is called once per frame
@@ -201,115 +150,19 @@ public class PhaseManager : MonoBehaviour
             if (currentPhase == Phase.Normal_Phase)
             {
                 Reticle.SetActive(true);
-                if (Line)
-                {
-                    Line.SetActive(false);
-                    LineAnime.SetBool("isMove", false);
-                }
-                Blur.strength.value = 0.0f;
             }
 
             if (currentPhase == Phase.Speed_Phase)
             {
                 SoundManager.instance.PlaySE("HighSpeed");
                 Reticle.SetActive(false);
-                if (Line)
-                {
-                    Line.SetActive(true);
-                    LineAnime.SetBool("isMove", true);
-                }
+                
                 vibrationManager.GetComponent<VibrationManager>().StartCoroutine("PlayVibration", "HighSpeed");
-
-                i = 0;
             }
 
             if (currentPhase == Phase.Attack_Phase)
             {
                 Reticle.SetActive(true);
-                if (Line)
-                {
-                    Line.SetActive(false);
-                    LineAnime.SetBool("isMove", false);
-                }
-
-                i = 0;
-            }
-        }
-
-        // フェーズ変更後の処理
-        if (i < BlurCount && volume != null)
-        {
-            if (currentPhase == Phase.Speed_Phase)
-            {
-                if (ManeverEnd == true)
-                {
-                    if (volume)
-                    {
-                        Blur.strength.value -= CountPoint * 5;
-                    }
-                    i+= 5;
-                }
-                else
-                {
-                    // ブラーをだんだんとかけていく処理
-                    if (gBlur.SamplingDistance.value != gBlurStrength)
-                    {
-                        if (volume)
-                        {
-                            gBlur.SamplingDistance.value += CountPoint;
-                        }
-                        i++;
-                    }
-                }
-
-
-                if(Blur.strength.value == BlurStrength && volume != null)
-                {
-                    Debug.Log("ブラー完了");
-                }
-            }
-            if (currentPhase == Phase.Attack_Phase && volume != null)
-            {
-                if (ManeverEnd == true)
-                {
-                    // マニューバ後のブラーを消していく処理
-                    Blur.strength.value -= CountPoint * 5;
-                    i += 5;
-                }
-                else
-                {
-                    //// ブラーをだんだんと0にしていく処理
-                    gBlur.SamplingDistance.value -= CountPoint;
-                    i += 2;
-                }
-            }
-        }
-        else if (currentPhase == Phase.Speed_Phase || currentPhase == Phase.Attack_Phase && volume != null)
-        {
-            if (currentPhase == Phase.Speed_Phase)
-            {
-                Blur.strength.value = 0.0f;
-                ManeverEnd = false;
-            }
-            if (currentPhase == Phase.Attack_Phase)
-            {
-                Blur.strength.value = 0.0f;
-                gBlur.SamplingDistance.value = 0.0f;
-                ManeverEnd = false;
-            }
-        }
-
-        // マニューバが行われたとき
-        if (playerMove != null)
-        {
-            if (playerMove.maneverFlg == true)
-            {
-                i = 0;
-                if (volume)
-                {
-                    Blur.strength.value = BlurStrength;
-                }
-                ManeverEnd = true;
             }
         }
     }
