8b2d27372185ec66944015cb41dc74c8ad508c31
Hirate_Iori <benizakura66613@gmail.com>
2023-05-15 14:51:55 +0900

    マネージャーシーン実装

--- Assets/Script/moriken/Damage.cs
+++ Assets/Script/moriken/Damage.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
-    public float PlayerDamage;
-    public float EnemyDamage;
+    public float PlayerDamage { get; private set; }
+    public float EnemyDamage { get; private set; }
+
+    // ダメージ名
+    [Header("ダメージ名")]
+    [Tooltip("プレイヤーへのダメージ")]
+    [SerializeField] DamageManager.DAMAGE_NAME playerDamageName = DamageManager.DAMAGE_NAME.E_DAMAGE_MAX;
+    [Tooltip("ボスへのダメージ")]
+    [SerializeField] DamageManager.DAMAGE_NAME bossDamageName = DamageManager.DAMAGE_NAME.E_DAMAGE_MAX;
+
+    // ダメージマネージャーオブジェクト
+    private GameObject DamageAmount;
+
+    private void Start()
+    {
+        // ダメージマネージャーを取得
+        DamageAmount = GameObject.Find("DamageManager");
+        //---- インスペクターで設定した与えるダメージを代入 ----
+        // プレイヤーへのダメージ
+        PlayerDamage = DamageAmount.GetComponent<DamageManager>().GetDamage(playerDamageName);
+        // ボスへのダメージ
+        EnemyDamage = DamageAmount.GetComponent<DamageManager>().GetDamage(bossDamageName);
+    }
 }
