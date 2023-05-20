3fd9cc34d67146b7b578af85ee3a71ca22b7e52d
Taisei12345 <tai.tai.717.tai.tai.817@gmail.com>
2023-05-20 20:41:06 +0900

    音声追加

--- Assets/Script/TAKAKI/Stage1TextWriter.cs
+++ Assets/Script/TAKAKI/Stage1TextWriter.cs
@@ -36,29 +36,37 @@ public class Stage1TextWriter : MonoBehaviour
         uitext.DrawNameText("≪ 司令官 ≫", "改めて作戦内容を伝える");
         yield return new WaitForSeconds(3.0f);
 
+        SoundManager.instance.PlayVOICE("0-2");
         uitext.DrawNameText("≪ 司令官 ≫", "強力なビーム兵器「衛星軌道砲」により、敵巨大飛行物体を\n撃墜するのが本ミッションの目的である。");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(8.0f);
 
+        SoundManager.instance.PlayVOICE("0-3");
         uitext.DrawNameText("≪ 司令官 ≫", "「衛星軌道砲」を当てるためには、正確な照準とエネルギー充填までヤツに気づかれない必要がある。");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(8.0f);
 
+        SoundManager.instance.PlayVOICE("0-4");
         uitext.DrawNameText("≪ 司令官 ≫", "ヤツに接近しての照準補佐と、注意を引き続けてもらうことが\n君に課せられた任務だ。");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(7.0f);
 
+        SoundManager.instance.PlayVOICE("0-5");
         uitext.DrawNameText("≪ 司令官 ≫", "唯一、それを可能にするのが君の搭乗している\n「ブルーアサルト」である。");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(6.0f);
 
+        SoundManager.instance.PlayVOICE("0-6");
         uitext.DrawNameText("≪ 司令官 ≫", "苛烈な攻撃が予想されるが、音速戦闘とバリアフィールドを搭載する\nその機体であれば切り抜けることが可能なはずだ。");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(9.5f);
 
+        SoundManager.instance.PlayVOICE("0-7");
         uitext.DrawNameText("≪ 司令官 ≫", "ヤツの注意をひくため、一秒でも長く戦闘を継続せよ。");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(5.5f);
 
+        SoundManager.instance.PlayVOICE("0-8");
         uitext.DrawNameText("≪ 司令官 ≫", "以上だ。健闘を祈る。");
         yield return new WaitForSeconds(3.0f);
 
+        SoundManager.instance.PlayVOICE("0-9");
         uitext.DrawNameText("≪ AI ≫", "まもなく敵巨大飛行物体の攻撃圏内に突入します。注意してください");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(6.0f);
 
         Window.SetActive(false);
         Name.SetActive(false);
--- Assets/Script/TAKAKI/Stage2TextWriter.cs
+++ Assets/Script/TAKAKI/Stage2TextWriter.cs
@@ -32,53 +32,69 @@ public class Stage2TextWriter : MonoBehaviour
 
         yield return new WaitForSeconds(1.0f);
 
+        SoundManager.instance.PlayVOICE("1-1");
         uitext.DrawNameText("≪ AI ≫", "衛星軌道砲、エネルギー充填完了まで10秒");
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(5.0f);
 
+        SoundManager.instance.PlayVOICE("1-2");
         uitext.DrawNameText("≪ AI ≫", "9");
         yield return new WaitForSeconds(1.0f);
 
+        SoundManager.instance.PlayVOICE("1-3");
         uitext.DrawNameText("≪ AI ≫", "8");
         yield return new WaitForSeconds(1.0f);
 
+        SoundManager.instance.PlayVOICE("1-4");
         uitext.DrawNameText("≪ AI ≫", "7");
         yield return new WaitForSeconds(1.0f);
 
+        SoundManager.instance.PlayVOICE("1-5");
         uitext.DrawNameText("≪ AI ≫", "6");
         yield return new WaitForSeconds(1.0f);
 
+        SoundManager.instance.PlayVOICE("1-6");
         uitext.DrawNameText("≪ AI ≫", "5");
         yield return new WaitForSeconds(1.0f);
 
+        SoundManager.instance.PlayVOICE("1-7");
         uitext.DrawNameText("≪ AI ≫", "4");
         yield return new WaitForSeconds(1.0f);
 
+        SoundManager.instance.PlayVOICE("1-8");
         uitext.DrawNameText("≪ AI ≫", "3");
         yield return new WaitForSeconds(1.0f);
 
+        SoundManager.instance.PlayVOICE("1-9");
         uitext.DrawNameText("≪ AI ≫", "2");
         yield return new WaitForSeconds(1.0f);
 
+        SoundManager.instance.PlayVOICE("1-10");
         uitext.DrawNameText("≪ AI ≫", "1");
         yield return new WaitForSeconds(1.0f);
 
+        SoundManager.instance.PlayVOICE("1-11");
         uitext.DrawNameText("≪ 司令官 ≫", "衛星軌道砲、撃て！");
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
 
+        SoundManager.instance.PlayVOICE("1-12");
         uitext.DrawNameText("≪ AI ≫", "目標への着弾を確認。目標を覆うバリアフィール\nいまだ健在です。");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(6.0f);
 
+        SoundManager.instance.PlayVOICE("1-13");
         uitext.DrawNameText("≪ 司令官 ≫", "次で仕留める。衛星軌道砲、再充填を開始しろ。");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(6.0f);
 
+        SoundManager.instance.PlayVOICE("1-14");
         uitext.DrawNameText("≪ 司令官 ≫", "だが、衛星の位置がヤツに補足された可能性がある。");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(5.0f);
 
+        SoundManager.instance.PlayVOICE("1-15");
         uitext.DrawNameText("≪ 司令官 ≫", "さらに接近し、ヤツの注意を引き付けてくれ！");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(3.5f);
 
+        SoundManager.instance.PlayVOICE("1-16");
         uitext.DrawNameText("≪ AI ≫", "敵巨大飛行物体に接近します。\nさらなる攻撃に注意してください。");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(6.0f);
 
         Window.SetActive(false);
         Name.SetActive(false);
