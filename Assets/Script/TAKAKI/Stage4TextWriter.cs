a6e2cba1ec0e094cc230ff00c2e29cf4a2c45e81
hami12 <s.shinya.1227@gmail.com>
2023-05-24 22:10:37 +0900

    ウィンドウ付け忘れ

--- Assets/Script/TAKAKI/Stage4TextWriter.cs
+++ Assets/Script/TAKAKI/Stage4TextWriter.cs
@@ -10,6 +10,8 @@ public class Stage4TextWriter : MonoBehaviour
     public GameObject Name;
     public GameObject Text;
 
+    private float Max_Height = 2.756843f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,9 +28,7 @@ public class Stage4TextWriter : MonoBehaviour
     // 文章を表示させるコルーチン
     IEnumerator Cotest()
     {
-        Window.SetActive(true);
-        Name.SetActive(true);
-        Text.SetActive(true);
+        StartCoroutine("WindowScaleUp");
 
         yield return new WaitForSeconds(1.0f);
 
@@ -48,9 +48,38 @@ public class Stage4TextWriter : MonoBehaviour
         uitext.DrawNameText("≪ AI ≫", "作戦終了の承認、確認しました。帰還しましょう。");
         yield return new WaitForSeconds(5.0f);
 
-        Window.SetActive(false);
+        StartCoroutine("WindowScaleDown");
+        //yield return null;
+    }
+    IEnumerator WindowScaleUp()
+    {
+        Window.SetActive(true);
+        Window.transform.localScale = new Vector3(
+            Window.transform.localScale.x,
+            0.0f,
+            Window.transform.localScale.z
+            );
+        for (float i = 1; i < 2; i += 0.1f)
+        {
+            Vector3 scale = Window.transform.localScale;
+            scale.y += Max_Height * 0.1f;
+            Window.transform.localScale = scale;
+            yield return new WaitForSeconds(0.01f);
+        }
+        Name.SetActive(true);
+        Text.SetActive(true);
+    }
+    IEnumerator WindowScaleDown()
+    {
         Name.SetActive(false);
         Text.SetActive(false);
-        //yield return null;
+        for (float i = 1; i < 2; i += 0.1f)
+        {
+            Vector3 scale = Window.transform.localScale;
+            scale.y -= Max_Height * 0.1f;
+            Window.transform.localScale = scale;
+            yield return new WaitForSeconds(0.01f);
+        }
+        Window.SetActive(false);
     }
 }
