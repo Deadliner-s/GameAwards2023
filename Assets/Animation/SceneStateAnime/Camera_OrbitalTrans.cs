1ba55c9158f7de94dc3ba0aaabcfd6aaef403d36
naoto12 <naoto_03_11m@yahoo.co.jp>
2023-05-27 02:46:02 +0900

    プロローグの遷移アニメーション完成

---
+++ Assets/Animation/SceneStateAnime/Camera_OrbitalTrans.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Camera_OrbitalTrans : MonoBehaviour
+{
+    private CinemachineVirtualCamera _virtualCamera;
+    private CinemachineOrbitalTransposer _orbitaltransposer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this._virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        this._orbitaltransposer = this._virtualCamera.GetComponentInChildren<CinemachineOrbitalTransposer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _orbitaltransposer.m_XAxis.Value += 0.1f;
+    }
+}
